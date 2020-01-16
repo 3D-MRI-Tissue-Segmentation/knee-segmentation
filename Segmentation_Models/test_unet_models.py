81ac08884b6b7fbb233c2fc2faa1aff211d35dc2
Joearrowsmith <joearrowsmith98@gmail.com>
2020-01-16 14:56:26 +0000

    Fixed unet get images

--- Segmentation_Models/test_unet_models.py
+++ Segmentation_Models/test_unet_models.py
@@ -1,10 +1,10 @@
 def test_standard_unet():
-    from src.data_gen.toy_data_gen import get_test_image
+    from src.data_gen.toy_data_gen import get_test_images
 
     n_images, n_reps, n_classes = 10, 4, 5
     width, height, depth = 400, 400, 3
-    images, one_hots = get_test_image(n_images, n_reps, n_classes, 
-                                    width, height, depth)
+    images, one_hots = get_test_images(n_images, n_reps, n_classes, 
+                                       width, height, depth)
 
     from src.model.unet import UNet
 
