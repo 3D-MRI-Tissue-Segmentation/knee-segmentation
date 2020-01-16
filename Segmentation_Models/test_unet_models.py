994a8df8e452a1763c1a4ebfcf742a62a39cc294
Joearrowsmith <joearrowsmith98@gmail.com>
2020-01-16 14:48:43 +0000

    modified test_unet to n images

--- Segmentation_Models/test_unet_models.py
+++ Segmentation_Models/test_unet_models.py
@@ -1,9 +1,9 @@
 def test_standard_unet():
     from src.data_gen.toy_data_gen import get_test_image
 
-    n_reps, n_classes = 4, 5
+    n_images, n_reps, n_classes = 10, 4, 5
     width, height, depth = 400, 400, 3
-    image, one_hot = get_test_image(n_reps, n_classes, 
+    images, one_hots = get_test_image(n_images, n_reps, n_classes, 
                                     width, height, depth)
 
     from src.model.unet import UNet
