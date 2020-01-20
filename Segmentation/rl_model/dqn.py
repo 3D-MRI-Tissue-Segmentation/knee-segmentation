58143a2084ccbb861678045be6a00887fe326601
Joearrowsmith <joearrowsmith98@gmail.com>
2020-01-20 15:05:14 +0000

    starting to add dqn

---
+++ Segmentation/rl_model/dqn.py
@@ -0,0 +1,58 @@
+import tensorflow as tf
+
+class DQNAgent:
+    ## Source keon: https://keon.github.io/deep-q-learning/
+
+    def __init__(self, state_size, action_size, memory_len, model,
+                 epsilon_decay=0.99, lr=0.001):
+        self.state_size = state_size
+        self.action_size = action_size
+        self.memory = deque(maxlen=1000)
+        self.gamma = 0.95    # discount rate
+        self.epsilon = 1.0   # exploration rate
+        self.epsilon_min = 0.01
+        self.epsilon_decay = epsilon_decay
+        self.learning_rate = lr
+        self.model = self._build_model(model)
+        self.target_model = self._build_model(model)
+        self.update_target_model()
+
+    def _build_model(self, model):
+        model.compile(loss=tf.keras.losses.Huber(),
+                      optimizer=Adam(lr=self.learning_rate))
+        return model
+
+    def update_target_model(self):
+        # copy weights from model to target_model
+        self.target_model.set_weights(self.model.get_weights())
+
+    def remember(self, state, action, reward, next_state, done):
+        self.memory.append((state, action, reward, next_state, done))
+
+    def act(self, state):
+        if np.random.rand() <= self.epsilon:
+            return random.randrange(self.action_size)
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])  # returns action
+
+    def replay(self, batch_size):
+        minibatch = random.sample(self.memory, batch_size)
+        for state, action, reward, next_state, done in minibatch:
+            target = self.model.predict(state)
+            if done:
+                target[0][action] = reward
+            else:
+                # a = self.model.predict(next_state)[0]
+                t = self.target_model.predict(next_state)[0]
+                target[0][action] = reward + self.gamma * np.amax(t)
+                # target[0][action] = reward + self.gamma * t[np.argmax(a)]
+            self.model.fit(state, target, epochs=1, verbose=0)
+        if self.epsilon > self.epsilon_min:
+            self.epsilon *= self.epsilon_decay
+
+    def load(self, name):
+        if os.path.isfile(name):
+            self.model.load_weights(name)
+
+    def save(self, name):
+        self.model.save_weights(name)
