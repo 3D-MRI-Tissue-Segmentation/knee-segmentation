03a8c0a20cb1fb7b2d7874411e9ca95e2e136906
Joearrowsmith <joearrowsmith98@gmail.com>
2020-02-05 20:18:43 +0000

    adding centre labeler with matplotlib

--- Segmentation/down_sampler/centre_point.py
+++ Segmentation/down_sampler/centre_point.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 from translate_image import pad_image, shift_image_and_label, rotate_image_and_label
 
 
-
 if __name__=="__main__":
     mnist = tf.keras.datasets.mnist
     (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
--- Segmentation/down_sampler/label_data.py
+++ Segmentation/down_sampler/label_data.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 
 class Label:
-    def __init__(self, image_height,
+    def __init__(self, train_images, image_height,
                  num_images=None, start_point=0,
                  data=tf.keras.datasets.mnist):
         self.num_images = num_images
@@ -11,7 +11,6 @@ class Label:
         self.data = data
         self.coords = []
 
-        (train_images, train_labels), (test_images, test_labels) = self.data.load_data()
         self.train_images = train_images
 
         self.image_counter = self.start_point
@@ -21,6 +20,8 @@ class Label:
         self.click_start = None
         self.layer_idx = 0
         self.layer_bounds = (0, image_height)
+
+        assert image_height == train_images.shape[1], "Unexpected shape for train image"
 
         self.done = False
 
@@ -36,8 +37,8 @@ class Label:
     def onrelease(self, event):
         end = (int(event.xdata), int(event.ydata))
         dist = Label.calc_drag_distance(self.click_start, end)
-        self.coords.append([self.image_counter, self.layer_idx,
-                            self.click_start, dist])
+        self.coords.append([self.image_counter, dist,
+                            *self.click_start, self.layer_idx])
         self.image_counter += 1
 
         end = False
@@ -49,48 +50,61 @@ class Label:
             self.update_image()
 
     def onarrowpress(self, event):
-        print(event.key)
         update = False
         if event.key == "up":
-            if self.layer_bounds[0] <= self.layer_idx < self.layer_bounds[1]:
+            if self.layer_bounds[0] <= self.layer_idx < (self.layer_bounds[1] - 1):
                 self.layer_idx += 1
                 update = True
-                print("increment")
         elif event.key == "down":
-            if self.layer_bounds[0] < self.layer_idx <= self.layer_bounds[1]:
+            if self.layer_bounds[0] < self.layer_idx <= (self.layer_bounds[1] - 1):
                 self.layer_idx -= 1
                 update = True
-                print("decrement")
         if update:
             self.update_image()
 
     def update_image(self):
-        self.im.set_data(self.train_images[self.image_counter])
+        self.im.set_data(self.train_images[self.image_counter][self.layer_idx, :, :])
         self.ax.set_title(f"Image being labeled: {self.image_counter}, layer: {self.layer_idx}")
         plt.draw()
 
     def reset(self):
         self.fig = plt.figure()
         self.ax = self.fig.gca()
-        self.im = self.ax.imshow(self.train_images[self.image_counter])
+        self.im = self.ax.imshow(self.train_images[self.image_counter][self.layer_idx, :, :])
 
     def start_loop(self):
         self.update_image()
-        self.im.set_data(self.train_images[self.image_counter])
+        self.im.set_data(self.train_images[self.image_counter][self.layer_idx, :, :])
         self.fig.canvas.mpl_connect('button_press_event', self.onclick)
         self.fig.canvas.mpl_connect('button_release_event', self.onrelease)
         self.fig.canvas.mpl_connect('key_press_event', self.onarrowpress)
         plt.show()
-        print("Done loop")
 
     def main(self):
         self.reset()
         self.start_loop()
 
 
-if __name__ == "__main__":
-    label = Label(4, start_point=4, num_images=4)
+def mnist_3d_main():
+    import h5py
+    with h5py.File("./Data/Tests_data/full_dataset_vectors.h5") as hf:
+        data = hf["X_train"][:]
+
+    data = np.reshape(data, (data.shape[0], 16, 16, 16))
+
+    label = Label(data, 16)
     label.main()
 
     for i in label.coords:
         print(i)
+
+    import pandas as pd
+
+    df = pd.DataFrame(np.asarray(label.coords),
+                      columns=["idx", "radius", "x", "y", "z"])
+    df.to_csv("./Data/mnist_3d_centre_test_data.csv", index=False)
+
+
+if __name__ == "__main__":
+    mnist_3d_main()
+    
