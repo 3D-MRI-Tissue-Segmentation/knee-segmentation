1cb2865a12e278295d0bc2b5f2f959f1aad8e0b9
laporastrelli <lr4617@ic.ac.uk>
2020-02-05 15:46:51 +0000

    Pulling

--- run_unet.py
+++ run_unet.py
@@ -5,7 +5,7 @@ dataset_size = 10
 batch_size = 5
 input_shape = (256, 256, 1)
 num_classes = 3
-output_shape = (256,256, num_classes)
+output_shape = (256, 256, num_classes)
 num_filters = 64 #number of filters at the start
 
 features = tf.random.normal((dataset_size,) + input_shape)
