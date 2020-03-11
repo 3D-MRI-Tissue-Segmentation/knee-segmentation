bcb0ab6d90cc3e28e5b6e61d79d45210026b6e24
Joe Arrowsmith <joearrowsmith98@gmail.com>
2020-03-11 00:02:44 +0000

    added time to train and deleted redunant file

--- Segmentation/model/vnet_train.py
+++ Segmentation/model/vnet_train.py
@@ -6,6 +6,7 @@ from tensorflow.keras.optimizers import Adam
 import matplotlib.pyplot as plt
 import pandas as pd
 import os
+import time
 
 
 def running_mean(x, N):
@@ -31,12 +32,11 @@ def setup_gpu():
 def train(model, n_classes=1, batch_size=1, sample_shape=(128, 128, 128), epochs=10,
           save_model=True, validate=True, train_name="",
           reduce_lr=False, start_lr=5e-4, dataset_load_method=None,
-          shuffle_order=True,
-          normalise_input=True, remove_outliers=True,
-          transform_angle=False, transform_position=False,
-          get_slice=False, get_position=False, skip_empty=True,
-          examples_per_load=1,
+          shuffle_order=True, normalise_input=True, remove_outliers=True,
+          transform_angle=False, transform_position="normal",
+          skip_empty=True, examples_per_load=1,
           **model_kwargs):
+    start_time = time.perf_counter()
     get_position, get_slice = False, False
     if model == "tiny":
         from Segmentation.model.vnet_tiny import VNet_Tiny
@@ -153,6 +153,7 @@ def train(model, n_classes=1, batch_size=1, sample_shape=(128, 128, 128), epochs
     else:
         history_1 = vnet.fit(x=tdataset, callbacks=callbacks, epochs=epochs, verbose=1)
 
+    time_taken = time.perf_counter() - start_time
     roll_period = 5
     f, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
     ax1.plot(history_1.history['loss'], label="loss")
@@ -171,9 +172,9 @@ def train(model, n_classes=1, batch_size=1, sample_shape=(128, 128, 128), epochs
     ax2.set_xlabel("epoch")
     ax2.set_ylabel("crossentropy")
     ax2.legend()
-    f.suptitle(f"{model}: {train_name}")
+    f.suptitle(f"{model}: {train_name}, {time_taken}")
     plt.savefig(f"checkpoints/train_session_{now_time}_{model}/train_result_{now_time}")
-
+    return time_taken
 
 if __name__ == "__main__":
 
@@ -183,16 +184,21 @@ if __name__ == "__main__":
     import sys
     sys.path.insert(0, os.getcwd())
 
-    e = 1
-    examples_per_load = 1
+    e = 2
+    examples_per_load = 2
+    batch_size = 6
 
-    train("tiny", sample_shape=(200, 200, 160), epochs=e, examples_per_load=examples_per_load,
-          train_name="tiny (200,200,160)")
-    train("small", sample_shape=(200, 200, 160), epochs=e, examples_per_load=examples_per_load,
-          train_name="small (200,200,160)")
-    train("small_relative", sample_shape=(200, 200, 160), epochs=e, examples_per_load=examples_per_load,
-          train_name="small_relative (200,200,128) (add)", action="add")
-    train("slice", sample_shape=(384, 384, 7), epochs=e, examples_per_load=examples_per_load,
-          train_name="slice (384,384,7) lr=5e-4, k=(3,3,3)", kernel_size=(3, 3, 3))
-    train("slice", sample_shape=(384, 384, 7), epochs=e, examples_per_load=examples_per_load,
-          train_name="slice (384,384,7) lr=5e-4, k=(3,3,1)", kernel_size=(3, 3, 1))
+    t0 = train("tiny", batch_size=batch_size, sample_shape=(200, 200, 160), epochs=e, examples_per_load=examples_per_load,
+               train_name="tiny (200,200,160)")
+
+    t1 = train("small", batch_size=batch_size, sample_shape=(200, 200, 160), epochs=e, examples_per_load=examples_per_load,
+               train_name="small (200,200,160)")
+
+    t2 = train("small_relative", batch_size=batch_size, sample_shape=(200, 200, 160), epochs=e, examples_per_load=examples_per_load,
+               train_name="small_relative (200,200,128) (add)", action="add")
+
+    t3 = train("slice", batch_size=batch_size, sample_shape=(384, 384, 7), epochs=e, examples_per_load=examples_per_load,
+               train_name="slice (384,384,7) lr=5e-4, k=(3,3,3)", kernel_size=(3, 3, 3))
+
+    t4 = train("slice", batch_size=batch_size, sample_shape=(384, 384, 7), epochs=e, examples_per_load=examples_per_load,
+               train_name="slice (384,384,7) lr=5e-4, k=(3,3,1)", kernel_size=(3, 3, 1))
