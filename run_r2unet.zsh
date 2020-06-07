08a034dc97831134811425a4ddecd4e5f65947c9
Olivia Gallupová <olivia.gallup@gmail.com>
2020-06-08 00:35:47 +0200

    olive run bash file

---
+++ run_r2unet.zsh
@@ -0,0 +1,7 @@
+python main.py --flagfile=config/train_gpu_oliv_lr1-3.cfg &&
+python main.py --flagfile=config/train_gpu_oliv_lr1-5.cfg &&
+python main.py --flagfile=config/train_gpu_oliv_lr5-5.cfg &&
+python main.py --flagfile=config/train_gpu_oliv_lr1-4.cfg &&
+python main.py --flagfile=config/train_gpu_oliv_lr5-4.cfg &&
+python main.py --flagfile=config/train_gpu_oliv_lr5-6.cfg
+
