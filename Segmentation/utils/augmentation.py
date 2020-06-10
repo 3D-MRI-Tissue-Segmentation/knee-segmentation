b4725b382ba51dba514c15db079a276dad9d00e0
Joe Arrowsmith <joearrowsmith98@gmail.com>
2020-06-10 22:23:56 +0100

    removed tfa

--- Segmentation/utils/augmentation.py
+++ Segmentation/utils/augmentation.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-import tensorflow_addons as tfa
+# import tensorflow_addons as tfa
 
 def flip_randomly_left_right_image_pair_2d(image_tensor, label_tensor):
 
@@ -15,40 +15,40 @@ def flip_randomly_left_right_image_pair_2d(image_tensor, label_tensor):
 
     return randomly_flipped_img, randomly_flipped_label
 
-def rotate_randomly_image_pair_2d(image_tensor, label_tensor, min_angle, max_angle):
+# def rotate_randomly_image_pair_2d(image_tensor, label_tensor, min_angle, max_angle):
 
-    random_var = tf.random.uniform(maxval=2, dtype=tf.int32, shape=[])
-    random_angle = tf.random.uniform(minval=min_angle, maxval=max_angle, dtype=tf.float32, shape=[])
+#     random_var = tf.random.uniform(maxval=2, dtype=tf.int32, shape=[])
+#     random_angle = tf.random.uniform(minval=min_angle, maxval=max_angle, dtype=tf.float32, shape=[])
 
-    randomly_rotated_img = tf.cond(pred=tf.equal(random_var, 0),
-                                   true_fn=lambda: tfa.image.rotate(image_tensor, random_angle),
-                                   false_fn=lambda: image_tensor)
+#     randomly_rotated_img = tf.cond(pred=tf.equal(random_var, 0),
+#                                    true_fn=lambda: tfa.image.rotate(image_tensor, random_angle),
+#                                    false_fn=lambda: image_tensor)
 
-    randomly_rotated_label = tf.cond(pred=tf.equal(random_var, 0),
-                                     true_fn=lambda: tfa.image.rotate(label_tensor, random_angle),
-                                     false_fn=lambda: label_tensor)
+#     randomly_rotated_label = tf.cond(pred=tf.equal(random_var, 0),
+#                                      true_fn=lambda: tfa.image.rotate(label_tensor, random_angle),
+#                                      false_fn=lambda: label_tensor)
 
-    randomly_rotated_label = one_hot_background_2d(randomly_rotated_label)
+#     randomly_rotated_label = one_hot_background_2d(randomly_rotated_label)
 
-    return randomly_rotated_img, randomly_rotated_label
+#     return randomly_rotated_img, randomly_rotated_label
 
-def translate_randomly_image_pair_2d(image_tensor, label_tensor, dx, dy):
+# def translate_randomly_image_pair_2d(image_tensor, label_tensor, dx, dy):
 
-    random_var = tf.random.uniform(maxval=2, dtype=tf.int32, shape=[])
-    random_dx = tf.random.uniform(minval=-dx, maxval=dx, dtype=tf.int32, shape=[])
-    random_dy = tf.random.uniform(minval=-dy, maxval=dy, dtype=tf.int32, shape=[])
+#     random_var = tf.random.uniform(maxval=2, dtype=tf.int32, shape=[])
+#     random_dx = tf.random.uniform(minval=-dx, maxval=dx, dtype=tf.int32, shape=[])
+#     random_dy = tf.random.uniform(minval=-dy, maxval=dy, dtype=tf.int32, shape=[])
 
-    randomly_translated_img = tf.cond(pred=tf.equal(random_var, 0),
-                                      true_fn=lambda: tfa.image.translate(image_tensor, [random_dx, random_dy]),
-                                      false_fn=lambda: image_tensor)
+#     randomly_translated_img = tf.cond(pred=tf.equal(random_var, 0),
+#                                       true_fn=lambda: tfa.image.translate(image_tensor, [random_dx, random_dy]),
+#                                       false_fn=lambda: image_tensor)
 
-    randomly_translated_label = tf.cond(pred=tf.equal(random_var, 0),
-                                        true_fn=lambda: tfa.image.translate(label_tensor, [random_dx, random_dy]),
-                                        false_fn=lambda: label_tensor)
+#     randomly_translated_label = tf.cond(pred=tf.equal(random_var, 0),
+#                                         true_fn=lambda: tfa.image.translate(label_tensor, [random_dx, random_dy]),
+#                                         false_fn=lambda: label_tensor)
 
-    randomly_translated_label = one_hot_background_2d(randomly_translated_label)
+#     randomly_translated_label = one_hot_background_2d(randomly_translated_label)
 
-    return randomly_translated_img, randomly_translated_label
+#     return randomly_translated_img, randomly_translated_label
 
 def one_hot_background_2d(label_tensor):
 
