f651a4fd20e52603d7e2f2b3fad3948ff3ded45d
Joe Arrowsmith <joearrowsmith98@gmail.com>
2020-06-12 16:51:30 +0100

    fixed typo

--- main.py
+++ main.py
@@ -320,14 +320,14 @@ def main(argv):
         plot_train_history_loss(history, multi_class=FLAGS.multi_class, savefig=training_history_dir)
     elif not FLAGS.visual_file == "":
         """ add visualisation code here """
-        path = os.path.join(FLAGS.logdir, FLAGS.tpu, FLAGS.visual_file, "*")
+        path = os.path.join(FLAGS.logdir, FLAGS.tpu, FLAGS.visual_file)
         print(path)
-        checkpoints = glob(path)
+        checkpoints = glob(os.path.join(path, "*"))
         print("+========================================================")
         print(checkpoints)
         print("+========================================================")
         from os import listdir
-        chkp = listdir(os.path.join(FLAGS.logdir, FLAGS.tpu, FLAGS.visual_file, "*"))
+        chkp = listdir(path)
         print(chkp)
         print("+========================================================")
     else:
