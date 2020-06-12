6a3110346c78d1abee32abdfe599ffb72759298c
pv09 <44298046+pv09@users.noreply.github.com>
2020-06-12 18:17:38 +0200

    used Path.glob instead of glob

--- main.py
+++ main.py
@@ -327,7 +327,7 @@ def main(argv):
 
         """ add visualisation code here """
         # path = os.path.join(FLAGS.logdir, FLAGS.tpu, FLAGS.visual_file)
-        print(path)
+        #print(path)
         # checkpoints = glob(os.path.join(path, "*"))
         print("+========================================================")
         print(checkpoints)
