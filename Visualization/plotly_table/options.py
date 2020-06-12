37dffb28d3ac2a68e2d5d351b16b61ee2d221b45
Olivia Gallupová <olivia.gallup@gmail.com>
2020-06-12 13:37:42 +0200

    plotly table making

---
+++ Visualization/plotly_table/options.py
@@ -0,0 +1,46 @@
+# Options for command line input
+import sys
+import argparse
+import os
+
+EXTENSIONS = ['.csv']
+
+def is_acceptable(filename):
+    return any(filename.endswith(extension) for extension in EXTENSIONS)
+
+
+# inspiration from https://github.com/neoamos/3d-pix2pix-CycleGAN/blob/master/options/base_options.py
+class Options():
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+        self.initialized = False
+
+    def initialize(self):
+        self.parser.add_argument('-in', '--input_data_path', type=str, default='', help='Path name of input csv.')
+        self.parser.add_argument('-test', '--test', type=bool, default=False, help='Generates smol csv file to display a dummy table.')
+        
+        self.initialized = True
+
+    def parse(self):
+        if not self.initialized:
+            self.initialize()
+        self.opt = self.parser.parse_args()
+
+        # check that the input is of an acceptable format
+        if self.opt.input_data_path:
+            assert is_acceptable(self.opt.input_data_path), 'Please input a file name of extension type ' + EXTENSIONS    
+
+        if self.opt.test:
+            import numpy as np
+            import pandas as pd
+
+            dummy_output_path = 'in.csv'
+            a = np.random.rand(10,3)
+            b = {1: a[:,0], 2: a[:,1], 3: a[:,2]}
+            c = pd.DataFrame(data=b)
+            c.to_csv(dummy_output_path, index=False)
+
+            self.opt.input_data_path = dummy_output_path
+
+
+        return self.opt
