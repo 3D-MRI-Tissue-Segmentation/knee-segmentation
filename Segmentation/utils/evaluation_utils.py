c748313d801667870081df490002d5d34a950670
pv09 <44298046+pv09@users.noreply.github.com>
2020-06-15 17:00:01 +0200

    figuring out directories

--- Segmentation/utils/evaluation_utils.py
+++ Segmentation/utils/evaluation_utils.py
@@ -66,135 +66,137 @@ def plot_and_eval_3D(trained_model,
         name = chkpt.split('/')[-1]
         name = name.split('.inde')[0]
 
-        print("\n\n\n\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-        print(f"\t\tLoading weights from {name.split('.')[1]} epoch")
-        print(f"\t\t\t{name}")
-        print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n\n")
+        if name.split('.')[1] == '002':
 
-        trained_model.load_weights('gs://' + os.path.join(bucket_name,
-                                                          weights_dir,
-                                                          tpu_name,
-                                                          visual_file,
-                                                          name)).expect_partial()
+            print("\n\n\n\n+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+            print(f"\t\tLoading weights from {name.split('.')[1]} epoch")
+            print(f"\t\t     {name}")
+            print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n\n")
 
-        pred_vols = []
-        y_vols = []
+            trained_model.load_weights('gs://' + os.path.join(bucket_name,
+                                                            weights_dir,
+                                                            tpu_name,
+                                                            visual_file,
+                                                            name)).expect_partial()
 
-        sample_x = []    # x for current 160,288,288 vol
-        sample_pred = []  # prediction for current 160,288,288 vol
-        sample_y = []    # y for current 160,288,288 vol
+            pred_vols = []
+            y_vols = []
 
-        for idx, ds in enumerate(dataset):
-            x, y = ds
-            batch_size = x.shape[0]
-            target = 160
-            print("Current batch size set to {}. Target depth is {}".format(batch_size, target))
+            sample_x = []    # x for current 160,288,288 vol
+            sample_pred = []  # prediction for current 160,288,288 vol
+            sample_y = []    # y for current 160,288,288 vol
 
-            x = np.array(x)
-            y = np.array(y)
+            for idx, ds in enumerate(dataset):
+                x, y = ds
+                batch_size = x.shape[0]
+                target = 160
+                print("Current batch size set to {}. Target depth is {}".format(batch_size, target))
 
-            pred = trained_model.predict(x)
-            print('Input image data type: {}, shape: {}'.format(type(x), x.shape))
-            print('Ground truth data type: {}, shape: {}'.format(type(y), y.shape))
-            print('Prediction data type: {}, shape: {}'.format(type(pred), pred.shape))
-            print("=================")
+                x = np.array(x)
+                y = np.array(y)
 
-            if (get_depth(sample_pred) + batch_size) < target:  # check if next batch will fit in volume (160)
-                sample_pred.append(pred)
-                sample_y.append(y)
-            else:
-                remaining = target - get_depth(sample_pred)
-                sample_pred.append(pred[:remaining])
-                sample_y.append(y[:remaining])
-                pred_vol = np.concatenate(sample_pred)
-                y_vol = np.concatenate(sample_pred)
-                sample_pred = [pred[remaining:]]
-                sample_y = [y[remaining:]]
+                pred = trained_model.predict(x)
+                print('Input image data type: {}, shape: {}'.format(type(x), x.shape))
+                print('Ground truth data type: {}, shape: {}'.format(type(y), y.shape))
+                print('Prediction data type: {}, shape: {}'.format(type(pred), pred.shape))
+                print("=================")
 
-                pred_vols.append(pred_vol)
-                y_vols.append(y_vol)
+                if (get_depth(sample_pred) + batch_size) < target:  # check if next batch will fit in volume (160)
+                    sample_pred.append(pred)
+                    sample_y.append(y)
+                else:
+                    remaining = target - get_depth(sample_pred)
+                    sample_pred.append(pred[:remaining])
+                    sample_y.append(y[:remaining])
+                    pred_vol = np.concatenate(sample_pred)
+                    y_vol = np.concatenate(sample_pred)
+                    sample_pred = [pred[remaining:]]
+                    sample_y = [y[remaining:]]
 
-                print("===============")
-                print("pred done")
-                print(pred_vol.shape)
-                print(y_vol.shape)
-                print("===============")
+                    pred_vols.append(pred_vol)
+                    y_vols.append(y_vol)
 
-                pred_vol_dice = dice_coef(y_vol, pred_vol)
+                    print("===============")
+                    print("pred done")
+                    print(pred_vol.shape)
+                    print(y_vol.shape)
+                    print("===============")
 
-                print("DICE:", pred_vol_dice)
+                    pred_vol_dice = dice_coef(y_vol, pred_vol)
 
-                # pred_vol = pred_vol[50:110, 114:174, 114:174, 0]
-                # pred_vol = np.stack((pred_vol,) * 3, axis=-1)
+                    print("DICE:", pred_vol_dice)
 
-                # fig = plot_volume(pred_vol)
-                # plt.savefig(f"results/hello-hello")
-                # plt.close('all')
+                    # pred_vol = pred_vol[50:110, 114:174, 114:174, 0]
+                    # pred_vol = np.stack((pred_vol,) * 3, axis=-1)
 
-                if idx == 2:
-                    print("Number of vols:", len(pred_vols), len(y_vols))
-                    batch_pred_vols = np.concatenate(pred_vols)
-                    batch_y_vols = np.concatenate(y_vols)
+                    # fig = plot_volume(pred_vol)
+                    # plt.savefig(f"results/hello-hello")
+                    # plt.close('all')
 
-                    print("BATCH pred SIZE:", batch_pred_vols.shape)
-                    print("BATCH y SIZE:", batch_y_vols.shape)
+                    if idx == 2:
+                        print("Number of vols:", len(pred_vols), len(y_vols))
+                        batch_pred_vols = np.concatenate(pred_vols)
+                        batch_y_vols = np.concatenate(y_vols)
 
-                    print("DICE BATCH:", dice_coef(batch_y_vols, batch_pred_vols))
+                        print("BATCH pred SIZE:", batch_pred_vols.shape)
+                        print("BATCH y SIZE:", batch_y_vols.shape)
 
-                    
+                        print("DICE BATCH:", dice_coef(batch_y_vols, batch_pred_vols))
 
-                print('is_multi_class', is_multi_class)
-                if is_multi_class:  # or np.shape(pred_vol)[-1] not
-                    pred_vol = np.argmax(pred_vol, axis=-1)
-                    print('pred_vol.shape', np.shape(pred_vol))
+                        
 
-
-                # Figure saving
-                # pred_vol = pred_vol[50:110, 114:174, 114:174]
-                # print(pred_vol.shape)
-                # fig_dir = "results"
-                # fig = plot_volume(pred_vol)
-                # print("shabem")
-                # plt.savefig(f"results/hello-hello2")
-                # plt.close('all')
-
-                # # Save volume as numpy file for plotlyyy
-                # vol_name_npy = os.path.join(fig_dir, (visual_file + "_" + str(idx)))
-                # np.save(pred_vol, vol_name_npy)
-                # print("npy saved as ", vol_name_npy)
-
-                #append image to use for gif
-                images_gif.append([ax.imshow(pred_vol[80,:,:]/6, cmap='gray', animated=True)])
-
-                # # Figure saving
-                # fig_dir = "results"
-                # fig = plot_volume(pred_vol)
-                # plt.savefig(f"results/hello-hello")
-                # plt.close('all')
-
-                # Save volume as numpy file for plotlyyy
-                # fig_dir = "results"
-                # vol_name_npy = os.path.join(fig_dir, (visual_file + "_" + str(idx)))
-                # print("npy save as ", vol_name_npy)
+                    print('is_multi_class', is_multi_class)
+                    if is_multi_class:  # or np.shape(pred_vol)[-1] not
+                        pred_vol = np.argmax(pred_vol, axis=-1)
+                        print('pred_vol.shape', np.shape(pred_vol))
 
 
-                # Get middle 60 slices cuz 288x288x160 too big
-                # d1,d2,d3 = np.shape(pred_vol)[0:3]
-                # d1, d2, d3 = int(np.floor(d1/2)), int(np.floor(d2/2)), int(np.floor(d3/2))
-                # roi = int(50 / 2)
-                # pred_vol_np = pred_vol[(d1-roi):(d1+roi),(d2-roi):(d2+roi), (d3-roi):(d3+roi)]
-                # np.save(vol_name_npy,pred_vol_np)
+                    # Figure saving
+                    # pred_vol = pred_vol[50:110, 114:174, 114:174]
+                    # print(pred_vol.shape)
+                    # fig_dir = "results"
+                    # fig = plot_volume(pred_vol)
+                    # print("shabem")
+                    # plt.savefig(f"results/hello-hello2")
+                    # plt.close('all')
+
+                    # # Save volume as numpy file for plotlyyy
+                    # vol_name_npy = os.path.join(fig_dir, (visual_file + "_" + str(idx)))
+                    # np.save(pred_vol, vol_name_npy)
+                    # print("npy saved as ", vol_name_npy)
+
+                    #append image to use for gif
+                    images_gif.append([ax.imshow(pred_vol[80,:,:]/6, cmap='gray', animated=True)])
+
+                    # # Figure saving
+                    # fig_dir = "results"
+                    # fig = plot_volume(pred_vol)
+                    # plt.savefig(f"results/hello-hello")
+                    # plt.close('all')
+
+                    # Save volume as numpy file for plotlyyy
+                    # fig_dir = "results"
+                    # vol_name_npy = os.path.join(fig_dir, (visual_file + "_" + str(idx)))
+                    # print("npy save as ", vol_name_npy)
 
 
-            break
+                    # Get middle 60 slices cuz 288x288x160 too big
+                    # d1,d2,d3 = np.shape(pred_vol)[0:3]
+                    # d1, d2, d3 = int(np.floor(d1/2)), int(np.floor(d2/2)), int(np.floor(d3/2))
+                    # roi = int(50 / 2)
+                    # pred_vol_np = pred_vol[(d1-roi):(d1+roi),(d2-roi):(d2+roi), (d3-roi):(d3+roi)]
+                    # np.save(vol_name_npy,pred_vol_np)
 
-#             if idx == 4:
-#                 break
-#             # we need to then merge into each (288,288,160) volume. Validation data should be in order
 
-            print("=================")
+                break
 
-       #  break
+    #             if idx == 4:
+    #                 break
+    #             # we need to then merge into each (288,288,160) volume. Validation data should be in order
+
+                print("=================")
+
+    #  break
 
 
     print("\n\n\n\n=================")
