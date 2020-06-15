483eca9b00743bb44a8fc649c64e7ee881a03841
pv09 <44298046+pv09@users.noreply.github.com>
2020-06-15 18:36:09 +0200

    added free tf variables

--- Segmentation/utils/evaluation_utils.py
+++ Segmentation/utils/evaluation_utils.py
@@ -73,7 +73,7 @@ def plot_and_eval_3D(trained_model,
             print(f"\t\t     {name}")
             print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n\n")
 
-            tf.disposeVariables()
+            tf.keras.backend.clear_session()
             trained_model.load_weights('gs://' + os.path.join(bucket_name,
                                                             weights_dir,
                                                             tpu_name,
