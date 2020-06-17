2a02867e096ffdd6073f6dfc0cd94ba77cd695ce
arahosu <arahosu@gmail.com>
2020-06-17 20:33:33 +0100

    reverting unet++_v2 to Pietro's version

--- Segmentation/model/unet.py
+++ Segmentation/model/unet.py
@@ -349,28 +349,38 @@ class Nested_UNet_v2(tf.keras.Model):
                                     padding='same',
                                     data_format=data_format)
 
-    def call(self, x, training=False):
+    def call(self, input, training=False):
 
-        # i + j = 0
-        x0_0 = self.conv_block_lists[0][0](x, training=training)
-        # i + j = 1
-        x1_0 = self.conv_block_lists[1][0](self.pool(x0_0), training=training)
-        x0_1 = self.conv_block_lists[0][1](tfkl.concatenate([x0_0, self.up(x1_0)]), training=training)
-        # i + j = 2
-        x2_0 = self.conv_block_lists[2][0](self.pool(x1_0), training=training)
-        x1_1 = self.conv_block_lists[1][1](tfkl.concatenate([x1_0, self.up(x2_0)]), training=training)
-        x0_2 = self.conv_block_lists[0][2](tfkl.concatenate([x0_0, x0_1, self.up(x1_1)]), training=training)
-        # i + j = 3
-        x3_0 = self.conv_block_lists[3][0](self.pool(x2_0), training=training)
-        x2_1 = self.conv_block_lists[2][1](tfkl.concatenate([x2_0, self.up(x3_0)]), training=training)
-        x1_2 = self.conv_block_lists[1][2](tfkl.concatenate([x1_0, x1_1, self.up(x2_1)]), training=training)
-        x0_3 = self.conv_block_lists[0][3](tfkl.concatenate([x0_0, x0_1, x0_2, self.up(x1_2)]), training=training)
-        # i + j = 4
-        x4_0 = self.conv_block_lists[4][0](self.pool(x3_0), training=training)
-        x3_1 = self.conv_block_lists[3][1](tfkl.concatenate([x3_0, self.up(x4_0)]), training=training)
-        x2_2 = self.conv_block_lists[2][2](tfkl.concatenate([x2_0, x2_1, self.up(x3_1)]), training=training)
-        x1_3 = self.conv_block_lists[1][3](tfkl.concatenate([x1_0, x1_1, x1_2, self.up(x2_2)]), training=training)
-        x0_4 = self.conv_block_lists[0][4](tfkl.concatenate([x0_0, x0_1, x0_2, x0_3, self.up(x1_3)]), training=training)
-        output = self.conv1x1(x0_4)
+        x = dict()
+        use_x = list()
+        x['0_0'] = self.conv_block_lists[0][0](input, training=training)
+        last_0_name = '0_0'
+        last_name = last_0_name
+
+        for sum in range(1, len(self.conv_block_lists)):
+            i, j = sum, 0
+            while j <= sum:
+
+                name = str(i) + '_' + str(j)
+
+                if i == sum:
+                    x[name] = self.conv_block_lists[i][j](self.pool(x[last_0_name]), training=training)
+                    last_0_name = name
+
+                else:
+                    for temp_right in range(0, j):
+                        string = str(i) + '_' + str(temp_right)
+                        use_x.append(x[string])
+
+                    use_x.append(self.up(x[last_name]))
+                    x[name] = self.conv_block_lists[i][j](tfkl.concatenate(use_x), training=training)
+
+                use_x.clear()
+                last = (i, j)
+                last_name = name
+                i = i - 1
+                j = j + 1
+
+        output = self.conv1x1(x[last_name])
 
         return output
