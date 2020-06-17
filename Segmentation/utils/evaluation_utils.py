77cfb0f50fd20e2c95ac903d4b1beff3bc722752
Olivia Gallupová <olivia.gallup@gmail.com>
2020-06-18 01:03:05 +0200

    saving tensorboard summaries locally

--- Segmentation/utils/evaluation_utils.py
+++ Segmentation/utils/evaluation_utils.py
@@ -271,6 +271,7 @@ def confusion_matrix(trained_model,
     # for n in f:
     #     writer_dir = os.path.join(writer_dir, n)
     # writer_dir = os.path.join(writer_dir, 'eval')
+    os.makedirs(writer_dir)
     eval_metric_writer = tf.summary.create_file_writer(writer_dir)
 
     if multi_class:
