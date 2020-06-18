cb0cefb2d35a73a6f385f0f485f09ecfcb097941
pv09 <44298046+pv09@users.noreply.github.com>
2020-06-18 15:23:05 +0200

    modified code to be one slice prediciton

--- Segmentation/utils/evaluation_utils.py
+++ Segmentation/utils/evaluation_utils.py
@@ -285,12 +285,13 @@ def epoch_gif(model,
 
                 if idx+1 == which_volume:
                     x, _ = ds
-                    x = np.array(x)
+                    # x = np.array(x)
+                    x_slice = np.array([x[which_slice-1]])
                     print('Input image data type: {}, shape: {}\n\n'.format(type(x), x.shape))
 
                     print('predicting slice {}'.format(which_slice))
                     # pred_vol = trained_model.predict(x)
-                    predicted_slice = trained_model.predict(x[which_slice-1])
+                    predicted_slice = trained_model.predict(x_slice)
                     if is_multi_class:
                         pred_vol = np.argmax(pred_vol, axis=-1)
                     print('slice predicted\n\n')
