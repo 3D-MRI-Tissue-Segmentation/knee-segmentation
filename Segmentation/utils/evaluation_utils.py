7bbcb521f82e680ec387dbc7b0684b5308ed439e
Olivia Gallupová <olivia.gallup@gmail.com>
2020-06-19 13:35:48 +0200

    increasing region of interest

--- Segmentation/utils/evaluation_utils.py
+++ Segmentation/utils/evaluation_utils.py
@@ -195,7 +195,7 @@ def plot_and_eval_3D(model,
 
 
                 # Get middle xx slices cuz 288x288x160 too big
-                roi = int(50 / 2)
+                roi = int(80 / 2)
                 d1,d2,d3 = np.shape(pred_vol)[0:3]
                 d1, d2, d3 = int(np.floor(d1/2)), int(np.floor(d2/2)), int(np.floor(d3/2))
                 pred_vol = pred_vol[(d1-roi):(d1+roi),(d2-roi):(d2+roi), (d3-roi):(d3+roi)]
