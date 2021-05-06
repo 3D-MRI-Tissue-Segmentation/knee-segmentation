2e39d2d80431e4ca8b119e8445f4ddf59e4d04ca
joearrowsmith <joearrowsmith98@gmail.com>
2021-05-06 13:23:34 +0100

    modifying for usage on local

--- create_tf_dataset.py
+++ create_tf_dataset.py
@@ -1,24 +1,24 @@
 from Segmentation.utils.data_loader import create_OAI_dataset
 import os
 
-def create_tfrecords(folder="train", use_2d=False, crop_size=None):
+def create_tfrecords(folder="train", use_2d=False, crop_size=None, mid_folders=""):
     train = (folder == 'train')
     str_dim = "" if use_2d else "_3d"
 
     if use_2d and crop_size is None:
         crop_size = 144
 
-    if not os.path.exists('./Data/tfrecords/'):
-        os.makedirs('./Data/tfrecords/')
-    if not os.path.exists(f'./Data/tfrecords/{folder}{str_dim}'):
-        os.makedirs(f'./Data/tfrecords/{folder}{str_dim}')
+    if not os.path.exists(f'./Data{mid_folders}/tfrecords/'):
+        os.makedirs(f'./Data{mid_folders}/tfrecords/')
+    if not os.path.exists(f'./Data{mid_folders}/tfrecords/{folder}{str_dim}'):
+        os.makedirs(f'./Data{mid_folders}/tfrecords/{folder}{str_dim}')
 
-    create_OAI_dataset(data_folder="./Data/" + folder,
-                       tfrecord_directory="./Data/tfrecords/" + folder + str_dim,
+    create_OAI_dataset(data_folder=f"./Data{mid_folders}/" + folder,
+                       tfrecord_directory=f"./Data{mid_folders}/tfrecords/" + folder + str_dim,
                        get_train=train,
                        use_2d=use_2d,
                        crop_size=crop_size)
     
 if __name__ == "__main__":
-    create_tfrecords("train")
-    create_tfrecords("valid")
+    # create_tfrecords("train", mid_folders='/mnt')
+    create_tfrecords("valid", mid_folders='/mnt')
